bf064c4cc2e151a2b4495eb20f0210460381d4a0
jasmo2 <jasmo2.com@gmail.com>
2016-09-22 14:45:43 -0500

    it2 TDD2 - Green

--- sequence.py
+++ sequence.py
@@ -6,6 +6,10 @@ class Sequence:
             seq_len = len(seq.split(','))
             return [seq_len]
 
+
     def min_el(self,seq):
         if seq == "":
             return [0, 0]
+        else:
+            seq_len = len(seq.split(','))
+            return [seq_len, 1]
