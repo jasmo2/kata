f408f153831a3ea67f83fa8730b7811002b4ec14
jasmo2 <jasmo2.com@gmail.com>
2016-09-22 15:11:40 -0500

    it4 TDD1 - Red

--- test_sequence.py
+++ test_sequence.py
@@ -1,23 +1,27 @@
 from unittest import TestCase
 from sequence import Sequence
 
- 
+
 class TestSequence(TestCase):
- def test_n_elements(self):
-     self.assertEqual([0], Sequence().sequence(""), "Empty Sequence")
-     self.assertEqual([1], Sequence().sequence("1"), "1 number Sequence")
-     self.assertEqual([2], Sequence().sequence("1,2"), "2 number Sequence")
-     self.assertEqual([4], Sequence().sequence("0,1,2,3"), "N number sequence")
+    def test_n_elements(self):
+        self.assertEqual([0], Sequence().sequence(""), "Empty Sequence")
+        self.assertEqual([1], Sequence().sequence("1"), "1 number Sequence")
+        self.assertEqual([2], Sequence().sequence("1,2"), "2 number Sequence")
+        self.assertEqual([4], Sequence().sequence("0,1,2,3"), "N number sequence")
 
- def test_min_element(self):
-     self.assertEqual([0,0], Sequence().min_el(""), "Empty Sequence min Elements")
-     self.assertEqual([1,1], Sequence().min_el("1"), "1 number Sequence min Elements")
-     self.assertEqual([2,1], Sequence().min_el("1,2"), "2 number Sequence min Elements")
-     self.assertEqual([5,3], Sequence().min_el("8,3,6,5,7"), "N number Sequence min Elements")
+    def test_min_element(self):
+        self.assertEqual([0,0], Sequence().min_el(""), "Empty Sequence min Elements")
+        self.assertEqual([1,1], Sequence().min_el("1"), "1 number Sequence min Elements")
+        self.assertEqual([2,1], Sequence().min_el("1,2"), "2 number Sequence min Elements")
+        self.assertEqual([5,3], Sequence().min_el("8,3,6,5,7"), "N number Sequence min Elements")
 
- def test_max_element(self):
-     self.assertEqual([0,0,0], Sequence().max_el(""), "Empty Sequence max Elements")
-     self.assertEqual([1,1,1], Sequence().max_el("1"), "1 Sequence max Elements")
-     self.assertEqual([2,1,2], Sequence().max_el("1,2"), "2 Sequence max Elements")
-     self.assertEqual([5,3,8], Sequence().max_el("8,3,6,5,7"), "N number Sequence min Elements")
+    def test_max_element(self):
+        self.assertEqual([0,0,0], Sequence().max_el(""), "Empty Sequence max Elements")
+        self.assertEqual([1,1,1], Sequence().max_el("1"), "1 Sequence max Elements")
+        self.assertEqual([2,1,2], Sequence().max_el("1,2"), "2 Sequence max Elements")
+        self.assertEqual([5,3,8], Sequence().max_el("8,3,6,5,7"), "N number Sequence min Elements")
+
+    def test_average(self):
+        self.assertEqual([0,0,0], Sequence().average_el(""), "Empty Sequence average Elements")
+
 
