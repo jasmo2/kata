bbf8f2f117aad70858df72f5d8cb7a1b62752daf
jasmo2 <jasmo2.com@gmail.com>
2016-09-22 14:31:29 -0500

    initial commit && it1 TDD3 - Red

--- test_sequence.py
+++ test_sequence.py
@@ -3,6 +3,7 @@ from sequence import Sequence
 
  
 class TestSequence(TestCase):
- def test_process(self):
+ def test_n_elements(self):
      self.assertEqual([0], Sequence().sequence(""), "Empty Sequence")
      self.assertEqual([1], Sequence().sequence("1"), "Empty Sequence")
+     self.assertEqual([2], Sequence().sequence("1,2"), "Empty Sequence")
