0d5b42e3da72961c3454700235c3ad99561bedd7
jasmo2 <jasmo2.com@gmail.com>
2016-09-22 14:35:32 -0500

    it1 TDD4 - Red

--- test_sequence.py
+++ test_sequence.py
@@ -5,5 +5,6 @@ from sequence import Sequence
 class TestSequence(TestCase):
  def test_n_elements(self):
      self.assertEqual([0], Sequence().sequence(""), "Empty Sequence")
-     self.assertEqual([1], Sequence().sequence("1"), "Empty Sequence")
-     self.assertEqual([2], Sequence().sequence("1,2"), "Empty Sequence")
+     self.assertEqual([1], Sequence().sequence("1"), "1 number Sequence")
+     self.assertEqual([2], Sequence().sequence("1,2"), "2 number Sequence")
+     self.assertEqual([4], Sequence().sequence("0,1,2,3"), "N number sequence")
